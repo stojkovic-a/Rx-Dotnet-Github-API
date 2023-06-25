342a1890a206dec2e50037219a520677b9246d0f
Aleksandar Stojkovic <aleksandarstojkovic@elfak.rs>
2023-06-25 21:12:55 +0200

    Working version no multithred

--- GithubAPI/HTTPServer.cs
+++ GithubAPI/HTTPServer.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace GithubSearch
 {
     public class HTTPServers
     {
+        private readonly int pageSize = 5;
+
+        private object fileWriteLock = new object();
+        private readonly string loggFileName = "Loggs.txt";
+
         private string address;
         private short Port;
         private HttpListener listener;
@@ -55,6 +64,11 @@ namespace GithubSearch
         public async Task HandleRequest(HttpListenerContext context)
         {
             string topic = string.Empty;
+            int minSize = 0;
+            int minStars = 0;
+            int minForks = 0;
+            int pageination = 0;
+            int pageNum = 0;
             bool valid = true;
             if (context.Request.HttpMethod != "GET") { valid = false; }
 
@@ -63,7 +77,7 @@ namespace GithubSearch
                 bool validPom = false;
                 foreach(var type in context.Request.AcceptTypes)
                 {
-                    if (type.Contains("image/gif") || type.Contains("*/*"))
+                    if (type.Contains("application/json") || type.Contains("*/*"))
                     {
                         validPom = true;
                         break;
@@ -76,28 +90,127 @@ namespace GithubSearch
                 valid = false;
             }
 
-            if (context.Request.RawUrl == null || context.Request.RawUrl == "/")
+            if (context.Request.RawUrl == null || context.Request.RawUrl == "/"||context.Request.QueryString.ToString()==null|| context.Request.QueryString.ToString()==string.Empty)
             {
                 valid = false;
             }
             else
             {
-               topic = context.Request.RawUrl;
-               topic = topic.Remove(0, 1);
+                var qs = context.Request.QueryString;
+                var parsed = qs;//HttpUtility.ParseQueryString(qs!);
+                if (parsed["topic"] != null && parsed["topic"]!=string.Empty)
+                {
+                    topic = parsed["topic"];
+                    minSize = Int32.Parse(parsed["size"] ?? "0");
+                    minStars = Int32.Parse(parsed["stars"] ?? "0");
+                    minForks = Int32.Parse(parsed["forks"] ?? "0");
+                    pageination = Int32.Parse(parsed["pging"] ?? "0");
+                    if (pageination == 1)
+                    {
+                        pageNum = Int32.Parse(parsed["pg"] ?? "1");
+                    }
+                    else
+                    {
+                        pageNum = 0;
+                    }
+                }
+                else
+                {
+                    valid = false;
+                }
+
             }
 
             if (valid)
             {
-                //SendRequest to api
+                var repoStream = new RepositoriumStream();
+
+                var observer1 = new RepositoriumObserver("Observer 1",context);
+
+                var filteredStream = repoStream.Where(r => r.Size > minSize && r.Forks > minForks && r.Stars > minStars);
+                if (pageination == 1) {
+                    filteredStream=filteredStream.Skip((pageNum-1)*pageSize).Take(pageSize);
+                }
+
+                var subscription1 = filteredStream
+                   // .SubscribeOn(Scheduler.Default)
+                    .Subscribe(observer1);
+
+                 await repoStream.GetRepositoriums(topic);
+                subscription1.Dispose();
             }
             else
             {
-               // Task t1 = Loggs(new object[] { false, false, false, false, context.Request });
-               // Response(400, context);
+                Task t1 = Loggs(false, false, context.Request );
+                Response(400, context);
                 Interlocked.Increment(ref numBadRequests);
             }
         }
 
+        public static void Response(int responseCode, HttpListenerContext context, int len = 0, string content = null)
+        {
+            string body = string.Empty;
+            var response = context.Response;
+            response.StatusCode = responseCode;
+
+            if (responseCode == 400)
+            {
+                response.ContentType = "text/html";
+                body = @"<html>
+                    <head><title>Bad Request</title></head>
+                    <body>Bad request was made</body>
+                                </html>";
+                try
+                {
+                    response.OutputStream.Write(Encoding.ASCII.GetBytes(body));
+                    response.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    return;
+                }
+                return;
+            }
+            else
+            if (responseCode == 404)
+            {
+                response.ContentType = "text/html";
+                body = @"<html>
+                    <head><title>Not Found</title></head>
+                    <body>Gif Not Found</body>
+                                </html>";
+                try
+                {
+                    response.OutputStream.Write(Encoding.ASCII.GetBytes(body));
+                    response.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    return;
+                }
+                return;
+            }
+            else
+            if (responseCode == 200)
+            {
+                response.ContentType = "text/plain";
+                response.ContentLength64 = len;
+                try
+                {
+                    response.OutputStream.Write(Encoding.ASCII.GetBytes(content));
+                    response.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    return;
+                }
+                return;
+            }
+
+        }
         public void Report()
         {
             Console.WriteLine($"{this.numOfRequests} requests were received");
@@ -105,6 +218,50 @@ namespace GithubSearch
             Console.WriteLine($"{this.numNotFoundReq} were not found");
             Console.WriteLine($"Of {this.numOfRequests - this.numBadRequests - this.numNotFoundReq} found requests," +
                 $" {this.numFoundInCache} were found in cache and {this.numFoundInDirectory} were found in directorty");
+        }
+
+        private async Task Loggs(bool valid, bool successful,HttpListenerRequest request)
+        {
+            string text = string.Empty;
+            if (!valid)
+            {
+                text = @"--Invalid request received at " + DateTime.Now.ToString() + "\n";
+            }
+            else
+            {
+                text = @"--Unkown error at" + DateTime.Now.ToString() + "\n";
+            }
+            text += $"Request:{request.HttpMethod} {request.RawUrl} {request.ProtocolVersion}\n";
+            text += "Accept: ";
+            if (request.AcceptTypes != null)
+            {
+                foreach (var types in request.AcceptTypes)
+                {
+                    text += types + " ";
+                }
+            }
+            text += "\n\n";
+
+
+            lock (fileWriteLock)
+                WriteFile(text);
+        }
+
+        private void WriteFile(string text)
+        {
+            if (!File.Exists(this.loggFileName))
+            {
+                File.Create(this.loggFileName);
+                TextWriter tw = new StreamWriter(this.loggFileName);
+                tw.Write(text);
+                tw.Close();
+            }
+            else if (File.Exists(this.loggFileName))
+            {
+                TextWriter tw = new StreamWriter(this.loggFileName, true);
+                tw.Write(text);
+                tw.Close();
+            }
         }
     }
 }
--- GithubAPI/Repositorium.cs
+++ GithubAPI/Repositorium.cs
@@ -1,6 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,4 +21,97 @@ namespace GithubSearch
         public int Stars;
         public int Forks;
     }
+
+    public class RepositoriumStream : IObservable<Repositorium>
+    {
+        private readonly Subject<Repositorium> repositoriumSubject;
+        private readonly IScheduler scheduler;
+        public RepositoriumStream()
+        {
+            repositoriumSubject = new Subject<Repositorium>();
+            scheduler = new EventLoopScheduler();
+        }
+
+        public async Task GetRepositoriums(string topic)
+        {
+            HttpClient client= new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.UserAgent.TryParseAdd("request");
+
+            var url = $"https://api.github.com/search/repositories?q=topic:{topic}";
+            
+           // _ = Task.Run(async () =>
+            await Task.Run(async () =>
+            {
+                Console.WriteLine("trying to enter try");
+                try
+                {
+                    var response = await client.GetAsync(url);
+                    Console.WriteLine("respones workds");
+                    response.EnsureSuccessStatusCode();
+                    Console.WriteLine("Ensure works");
+                    var content = await response.Content.ReadAsStringAsync();
+                    var repos = JsonConvert.DeserializeObject<dynamic>(content).items;
+                    Console.WriteLine("heyo");
+                    //Console.WriteLine(repos[0]);
+                    foreach (var r in repos)
+                    {
+                        var newRepo = new Repositorium
+                        {
+                            Name = r.name,
+                            Description = r.description,
+                            Forks = r.forks_count,
+                            Size = r.size,
+                            Stars = r.stargazers_count,
+                        };
+                        repositoriumSubject.OnNext(newRepo);
+                    }
+                    repositoriumSubject.OnCompleted();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Something went wrong");
+                    Console.WriteLine(ex);
+                    Console.WriteLine(ex.Message);
+                    repositoriumSubject.OnError(ex);
+                }
+            });
+        }
+
+        public IDisposable Subscribe(IObserver<Repositorium> observer)
+        {
+            return repositoriumSubject/*.ObserveOn(this.scheduler)*/.Subscribe(observer);
+        }
+    }
+
+    public class RepositoriumObserver : IObserver<Repositorium>
+    {
+        private readonly string name;
+        private string reply = string.Empty;
+        private readonly HttpListenerContext context;
+        private readonly IScheduler scheduler;
+
+        public RepositoriumObserver(string name,HttpListenerContext c)
+        {
+            this.name = name;
+            this.context = c;
+            scheduler = new EventLoopScheduler();
+        }
+        public void OnNext(Repositorium repo)
+        {
+            Console.WriteLine($"{name}: {repo.Name}");
+            this.reply += $"Name:{repo.Name}\nDescription:{repo.Description}\nSize:{repo.Size}\nStars:{repo.Stars}\n" +
+                $"Forks:{repo.Forks}\n\n";
+        }
+        public void OnError(Exception ex)
+        {
+            Console.WriteLine("sucker");
+        }
+        public void OnCompleted()
+        {
+            HTTPServers.Response(200, context, reply.Length, reply);
+            Console.WriteLine("Completed");
+        }
+    }
+
 }
